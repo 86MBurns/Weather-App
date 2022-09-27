4677bb7a2f4c8792180dbf91f706798a9cc980e1
Matt Burns <matt.burns01@gmail.com>
2022-09-27 14:32:33 -0700

    built fetch for geolocation and one call weather

--- assets/script.js
+++ assets/script.js
@@ -45,3 +45,45 @@ function todaysForcast(city, weather, time) {
     var uvindex = weather.uvi;
 
 }
+// submit the form to fetch weather info
+
+    // fetch the city name from text <input>
+
+    // Call the fetchGeolocation and pass the cityName
+
+// handle button clicks to fetch weather info
+
+    // get the city name from clicked button's (event.target) data-city attribute.
+
+    // Call the  `fetchGeolocation` and pass the ciy name
+
+
+
+
+
+    function fetchGeolocation (cityName) {
+
+        var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid={5d572e9f49dfb2520c4e7712e994e1fc}`;
+    
+        fetch(request)
+        .then(function(responce) {
+            return responce.json();
+        })
+        .then(function(data){
+            console.log(data);
+        });
+    }
+    
+    
+    // Fetch geolocation data (geocoding API)
+    
+    function fetchOneCallWeather(lat, lon){
+        var request = `https://api.openweathermap.org/data/2.5/onecall?appid=5d572e9f49dfb2520c4e7712e994e1fc&lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely`;
+        fetch(request)
+        .then(function(responce) {
+            return responce.json();
+        })
+        .then(function(data){
+            console.log(data);
+        });
+    }
